ae3f27e56a9988150ba485289a30ccc73f028ca1
Rodrigo Amaral <rodrigoamaral@gmail.com>
2014-02-12 17:51:54 -0300

    Mostra nickname do usuario logado

--- hello/main.py
+++ hello/main.py
@@ -1,8 +1,15 @@
+from google.appengine.api import users
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        user = users.get_current_user()
+
+        if user:
+            self.response.write('Hello %s!' % user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
